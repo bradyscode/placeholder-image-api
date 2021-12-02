8bc4ec407d4b3b1596b6d707cbbff980518d88c7
Brady <bradystohler15@gmail.com>
2021-12-02 17:45:51 -0500

    create image source interface and branch

---
+++ returnScaledImage/Interfaces/IImageSource.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace returnScaledImage.Interfaces
+{
+    public interface IImageSource
+    {
+        Task<List<Image>> GetImages();
+    }
+}
---
+++ returnScaledImage/Interfaces/ImageSources/NatureImageSource.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace returnScaledImage.Interfaces.ImageSources
+{
+    public class NatureImageSource : IImageSource
+    {
+        public Task<List<Image>> GetImages()
+        {
+
+        }
+    }
+}
